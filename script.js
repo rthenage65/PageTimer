db12c2bf94d95a4a044e67c8d92d8050f208a7e0
Richard Henage <rthenage@gmail.com>
2025-06-26 12:26:06 -0600

    Pause, restart, previous, and next buttons

--- script.js
+++ script.js
@@ -1,48 +1,97 @@
+﻿const pageNumberElement = document.getElementById('pageNumber');
+const progressLineElement = document.getElementById('progressLine');
+const pauseResumeElement = document.getElementById('pauseResumeButton');
+
+const pageTurnSound = new Audio('./page-turn.mp3');
+  
+let currentPage = 0;
+let endPage = 0;
+let currentTime = 0; // counts up in seconds until it reaches the required time to turn the page
+
+let playState = 'paused'
+
+let timerInterval;
+
+// this function resets the app to the specified start page and presses play
 document.getElementById('startBtn').addEventListener('click', function() {
   console.log(`startBtn`)
   const startPage = parseInt(document.getElementById('startPage').value);
-  const endPage = parseInt(document.getElementById('endPage').value);
+  endPage = parseInt(document.getElementById('endPage').value);
+
+  updatePage(startPage);
+  resume()
+});
+
+function updatePage(pageNumber) {
+    if (pageNumber > endPage) {
+        // alert('You have reached the last page.');
+        currentPage = endPage
+        pause()
+        return;
+    }
+
+    currentPage = pageNumber
+    currentTime = 0
+
+    pageTurnSound.play();
+
+    // Update page number display
+    pageNumberElement.textContent = 'Page: ' + currentPage;
+}
+
+function tickTimer() {
+  currentTime++
+
+  // Calculate the time required for the current page
   const timePerPage = document.getElementById('timePerPage').value.split(':');
   const minutes = parseInt(timePerPage[0]);
-  const seconds = parseInt(timePerPage[1]);
+  const seconds = parseInt(timePerPage[1]);  
+  const durationOfPage = (minutes * 60 + seconds);
 
-  let currentPage = startPage;
-  let position = 0;
-  
-  const pageNumberElement = document.getElementById('pageNumber');
-  const progressLineElement = document.getElementById('progressLine');
-  const pageTurnSound = new Audio('./page-turn.mp3');
-  pageTurnSound.play();
-
-  function updatePage() {
-      if (currentPage > endPage) {
-          alert('You have reached the last page.');
-          return;
-      }
-
-      // Update page number display
-      pageNumberElement.textContent = 'Page: ' + currentPage;
-      
-      // Reset progress line for the new page
-      progressLineElement.style.transition = `none`;
-      progressLineElement.style.height = '0';
-
-      // Calculate the time for the current page
-      const timeForCurrentPage = (minutes * 60 + seconds);
-
-      setTimeout(() => {
-        // Animate the progress line
-        progressLineElement.style.transition = `height ${timeForCurrentPage}s linear`;        
-        progressLineElement.style.height = '100%'; // Move to the bottom
-      }, 100)
-      
-      // Move to the next page after the set time
-      setTimeout(() => {
-          currentPage++;
-          position++;
-          pageTurnSound.play();
-          updatePage();
-      }, timeForCurrentPage * 1000);
+  if (currentTime >= durationOfPage) {
+      updatePage(currentPage + 1)
   }
-  updatePage();
-});
+
+  const currentPercentage = (100 * currentTime / durationOfPage).toFixed(1)
+  progressLineElement.style.height = currentPercentage + '%'; // Move to the bottom
+}
+
+function resume() {
+  playState = 'running'
+  clearInterval(timerInterval)
+  timerInterval = setInterval(tickTimer, 1000)
+
+  // update the pauseResume element
+  pauseResumeElement.innerHTML = '⏸'
+}
+
+function pause() {
+  playState = 'paused'
+  clearInterval(timerInterval)
+
+  // update the pauseResume element
+  pauseResumeElement.innerHTML = '⏵'
+}
+
+function pauseResumeButton() {
+    if (playState === 'running') {
+        pause()
+    } else {
+        resume()
+    }
+}
+
+function restartPageButton() {
+    currentTime = -1 // will jump to 0 when tickTimer runs
+    tickTimer()
+}
+
+function previousPageButton() {
+    updatePage(currentPage - 1)
+    restartPageButton()
+}
+
+function nextPageButton() {
+    updatePage(currentPage + 1)
+    restartPageButton()
+}
